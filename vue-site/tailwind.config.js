3f1142d974e8d5fc9661eacad3842937b68e494a
silvomor <silvomor@gmail.com>
2021-08-03 23:40:39 +0530

    Nav Bar is working now for mobile and desktops

--- vue-site/tailwind.config.js
+++ vue-site/tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navBagCol: '#1F2937',
+        webBagCol: '#4B5563',
+      },
+    },
   },
   variants: {
     extend: {},
