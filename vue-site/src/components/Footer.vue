acb9f2813322fa49a023745745252a7cd1c95721
silvomor <silvomor@gmail.com>
2021-08-04 04:06:42 +0530

    Nav and Footer View added

---
+++ vue-site/src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <footer class="absolute bottom-0 flex w-screen justify-center px-4 text-gray-800 bg-gray-400 dark:text-white dark:bg-gray-800">
+  <div class="container py-6">
+    <hr class="h-px mt-6 border-gray-300 border-none dark:bg-gray-700" />
+
+    <div class="flex flex-col items-center justify-between mt-6 md:flex-row">
+      <div>
+        <a href="#" class="text-xl font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300">Brand</a>
+      </div>
+
+      <div class="flex mt-4 md:m-0">
+        <div class="-mx-4">
+          <a href="#" class="px-4 text-sm font-medium text-gray-800 dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 hover:underline">About</a>
+          <a href="#" class="px-4 text-sm font-medium text-gray-800 dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 hover:underline">Blog</a>
+          <a href="#" class="px-4 text-sm font-medium text-gray-800 dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 hover:underline">News</a>
+          <a href="#" class="px-4 text-sm font-medium text-gray-800 dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 hover:underline">Contact</a>
+        </div>
+      </div>
+    </div>
+    <hr class="h-px my-6 bg-gray-300 border-none dark:bg-gray-700" />
+
+    <div>
+      <p class="text-center text-gray-800 dark:text-white">© Brand 2020 - All rights reserved</p>
+    </div>
+  </div>
+</footer>
+</template>
--- vue-site/src/views/Home.vue
+++ vue-site/src/views/Home.vue
@@ -1,18 +1,8 @@
 <template>
   <div class="home">
     <div class="animate-bounce"><img class="mx-auto h-24 transform rotate-180" alt="Vue logo" src="../assets/logo.png"></div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
 </script>
